247502f1349ace94d354a413506edb32a6e320ac
Don Schrimsher <don4thirteen@gmail.com>
2021-11-09 10:38:15 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 # This file contains all of the necessary steps to setup a new laptop
 
+# Clone and setup the dotfiles repo (this repo) that contains all of the setup scripts
+mkdir -p Development.nosync/Personal
+cd Development.nosync/Personal
+git clone git@github.com:theotherdon/dotfiles.git
+cd dotfiles
+cat .vimrc > ~/.vimrc
+cat .zshrc > ~/.zshrc
+
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
